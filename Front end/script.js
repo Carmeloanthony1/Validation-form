ed7d8e0f0e50efcb2a373a9c4ff917180473c74b
Retry30 <meloproacc@gmail.com>
2026-05-14 07:49:13 -0400

    fix : fix code agar bisa di submit

--- Front end/script.js
+++ Front end/script.js
@@ -1,28 +1,37 @@
 const form = document.getElementById("Identitas");
 const nama = document.querySelector('input[name="nama"]');
-const phonenumber = document.querySelector('input[name="kontak"');
+const phonenumber = document.querySelector('input[name="kontak"]');
 const email = document.querySelector('input[name="email"]');
 const selectJurusan = document.getElementById('jurusan'); 
 
-const validation_nama = nama.value.trim(); //ngilangin spasi
-const validation_phonenumber = phonenumber.value.trim();
-const regexhp = /^(\+62|0|62)\d{9,12}$/;
-const regexemail = /[\w._]+@[a-z._].+\.[a-z]+/;
-if(nama.length < 3){
-    alert("Nama terlalu pendek");
-    return;
-}
-if(/\d/.test(nama)){
-    alert("Tidak boleh ada angka");
-    return;
-}
+form.addEventListener('submit', (e) => {
+    const validation_nama = nama.value.trim(); //ngilangin spasi
+    const validation_phonenumber = phonenumber.value.trim();
+    const validation_email = email.value.trim();
+    if(validation_nama.length < 3){
+        alert("Nama terlalu pendek");
+        e.preventDefault(); 
+        return;
+    }
 
-if(regexhp.test(phonenumber)){
-    alert("Nomor telfon harus minimal 11 digit atau maksimal 13 digit");
-    return;
-}
+    if(/\d/.test(validation_nama)){    
+        alert("Tidak boleh ada angka");
+        e.preventDefault();
+        return;
+    }
+    const regexhp = /^(\+62|0|62)\d{9,12}$/;
+    if(!regexhp.test(validation_phonenumber)){
+        alert("Nomor telfon harus minimal 11 digit atau maksimal 13 digit");
+        e.preventDefault();
+        return;
+    }
 
-if(regexemail.test(email)){
-    alert("Format email salah");
-    return;
-}
+    const regexemail = /[\w._]+@[a-z._].+\.[a-z]+/;
+    if(!regexemail.test(validation_email)){
+        alert("Format email salah");
+        e.preventDefault();
+        return;
+    }
+
+    alert("Berhasil di submit!");
+}); 
